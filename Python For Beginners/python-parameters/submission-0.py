2c199276c3a2aa069608f933c7649eee83f855c2
ganeshpradippatil-cmd <ganeshpradippatil@gmail.com>
2026-08-04 01:19:52 +0530

    Add: python-parameters - submission-0

---
+++ Python For Beginners/python-parameters/submission-0.py
@@ -0,0 +1,18 @@
+def farewell(name):
+    msg = "Goodbye, " + name
+    print(msg)
+
+def farewell(name):
+    msg = "Goodbye, " + name
+    print(msg)
+
+def farewell(name):
+    msg = "Goodbye, " + name
+    print(msg)
+
+   
+
+# don't modify below this line
+farewell("Bob")
+farewell("Charlie")
+farewell("NeetCode")
