df683186dbbbadf306840e90e1fa02602809f174
ganeshpradippatil-cmd <ganeshpradippatil@gmail.com>
2026-08-13 14:47:48 +0530

    Add: python-read-input-practice - submission-1

---
+++ Python For Beginners/python-read-input-practice/submission-1.py
@@ -0,0 +1,9 @@
+def add_two_numbers() -> int:
+  first, second = map(int, input().split(","))
+  return first + second
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
